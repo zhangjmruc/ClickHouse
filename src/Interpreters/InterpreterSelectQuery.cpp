0804d852ce5ad448ede5beaf50086da20c1f034c
jianmei zhang <zhangjmruc@sina.com>
2022-06-21 11:07:25 +0800

    Disable optimizations for count() when lightweight delete exists, add hasLightweightDelete() function in IMergeTreeDataPart

--- src/Interpreters/InterpreterSelectQuery.cpp
+++ src/Interpreters/InterpreterSelectQuery.cpp
@@ -1935,6 +1935,7 @@ void InterpreterSelectQuery::executeFetchColumns(QueryProcessingStage::Enum proc
         && !settings.allow_experimental_query_deduplication
         && storage
         && storage->getName() != "MaterializedMySQL"
+        && !storage->hasLightweightDelete()
         && !row_policy_filter
         && processing_stage == QueryProcessingStage::FetchColumns
         && query_analyzer->hasAggregation()
